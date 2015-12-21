03767538bcc1aea9cdf3b6be76426a6cab2e905f
Oleg Perekhodov <hariko.storm@gmail.com>
2015-12-22 01:06:10 +0600

    third commit

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!3</h1>
+        <h1>Congratulations!hello</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
